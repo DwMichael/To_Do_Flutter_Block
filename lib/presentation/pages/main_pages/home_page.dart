00cba5c6c68f40858fd4f569486177c7e9db817e
Michael <dwernicki.michal@gmail.com>
2023-10-11 21:17:15 +0200

    CounterBloc

--- lib/presentation/pages/main_pages/home_page.dart
+++ lib/presentation/pages/main_pages/home_page.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:to_do_app/business_logic/bloc/conectivity_cubit.dart';
+
 import 'package:to_do_app/business_logic/bloc/counterbloc.dart';
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key, required this.title});
 
   final String title;
-
 
   @override
   Widget build(BuildContext context) {
@@ -17,26 +15,29 @@ class MyHomePage extends StatelessWidget {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(title),
       ),
-      body:
-         Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'You have pushed the button this many times:',
-              ),
-              Text(
-                bloc.stream..toString(),
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-            ],
-          ),
-         ),
-      
-    floatingActionButton: FloatingActionButton(onPressed: (){
-  bloc.add(CounterIncrementPressed());
-    }),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            StreamBuilder<int>(
+              initialData: 0,
+              stream: bloc.stream,
+              builder: (context, snapshot) {
+                return Text(
+                  '${snapshot.data}',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: () {
+        bloc.add(CounterIncrementPressed());
+      }),
     );
-    
   }
 }
